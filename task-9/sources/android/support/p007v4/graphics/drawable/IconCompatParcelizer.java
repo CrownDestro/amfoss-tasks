4844c554ec2e1dd16673e1438bf30edeb3c34281
CrownDestro <144609111+CrownDestro@users.noreply.github.com>
2023-10-13 21:25:05 +0530

    Add files via upload

---
+++ task-9/sources/android/support/p007v4/graphics/drawable/IconCompatParcelizer.java
@@ -0,0 +1,15 @@
+package android.support.p007v4.graphics.drawable;
+
+import androidx.core.graphics.drawable.IconCompat;
+import androidx.versionedparcelable.C1188a;
+
+/* renamed from: android.support.v4.graphics.drawable.IconCompatParcelizer */
+public final class IconCompatParcelizer extends androidx.core.graphics.drawable.IconCompatParcelizer {
+    public static IconCompat read(C1188a aVar) {
+        return androidx.core.graphics.drawable.IconCompatParcelizer.read(aVar);
+    }
+
+    public static void write(IconCompat iconCompat, C1188a aVar) {
+        androidx.core.graphics.drawable.IconCompatParcelizer.write(iconCompat, aVar);
+    }
+}
